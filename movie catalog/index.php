9a333fac075c96d37f98af970cea9915721e8a3e
AB <alexandrubarbanoua14.3.6@gmail.com>
2023-01-28 12:15:45 +0200

    movie catalog its not done yet

---
+++ movie catalog/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cineplex 2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+            <div class="movies_fimls_item">
+                <img src="https://content-service.cineplex.md/thumbnail/_/263/390/crop/good/7441edcb-9ed0-5cc4-af95-2ac22e6c912f.jpg" class="image">
+                <div class="movies_overlay">
+                    <div class="overlay_content"> 
+                    <h3 class="overlay__title">Babylon2D</h3> 
+                <!-- <img src="https://cineplex.md/img/line.png" alt="">  -->
+                    <div class="overlay__genre">Comedie</div> 
+                    <div class="overlay__age">16 +</div>
+                    <div class="overlay__length">188 minute</div> 
+                    <span class="overlay__lang">(RU)</span> 
+                    <div class="overlay__length">(19.01.2023)</div> 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
